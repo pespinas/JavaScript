ca501f708f8c04c33bdcb83d34d1e8557bcfa05b
pespinas <77834644+pespinas@users.noreply.github.com>
2023-03-10 12:43:51 +0100

    Add files via upload

---
+++ Ej20/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    let estadoComida='100';
+    let estadoJuego='100';
+    let estadoLucha='100';
+    let estadoDormir='100';
+    
+    $('#comer1','#comer2','#comer3').click(function() {
+    });
+
+    $('#jugar1','#jugar2').click(function() {
+        
+    });
+    $('#luchar1','#luchar2').click(function() {
+        
+    });
+    $('#dormir1','#dormir2').click(function() {
+        
+    });
+        
+    function comer(fruta) {
+        switch (fruta) {
+            case "zydra":
+                
+                break;
+            case "manzana":
+                
+                break;
+            case "meloc":
+                
+                break; 
+            default:
+
+                break;
+        }
+    }
+    function jugar(juego) {
+       if(juego == "furbo"){
+
+       }
+       else{
+
+       }
+    }
+    function luchar(lucha) {
+        if(lucha == "boxing"){
+ 
+        }
+        else{
+ 
+        }
+     }
+     function dormir(dormir) {
+        if(dormir == "siesta"){
+ 
+        }
+        else{
+ 
+        }
+     }
+
+});
